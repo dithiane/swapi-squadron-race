f916b57a1063ac6b61b9e390e8ce76a4646f6273
dithiane <elenamena.colorado@gmail.com>
2023-05-23 13:16:02 -0600

    Added static

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 //Middleware
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public'))
 })
 
 app.use(cors());
